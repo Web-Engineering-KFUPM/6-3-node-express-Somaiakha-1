21ce3465f1a42fbac02a938900ede9fb0534f49e
SomaiaKha <s202257000@kfupm.edu.sa>
2026-04-06 11:12:05 +0300

    TODO

--- 6-3-node-express/backend/quotes.js
+++ 6-3-node-express/backend/quotes.js
@@ -1,12 +1,13 @@
-// Import random.js file
+import { getRandomInt } from "./utils/random.js";
 
 const quotes = [
-  "Code is like humor. When you have to explain it, it’s bad.",
-  "Before software can be reusable it first has to be usable.",
-  "Make it work, make it right, make it fast.",
-  "Programming isn’t about what you know; it’s about what you can figure out."
+  "The best way to get started is to quit talking and begin doing.",
+  "Don't let yesterday take up too much of today.",
+  "It's not whether you get knocked down, it's whether you get up.",
+  "If you are working on something exciting, it will keep you going.",
 ];
 
-// TODO 3: Create and export getRandomQuote function
-//Hint: export function getRandomQuote(){}
-
+export function getRandomQuote() {
+  const index = getRandomInt(quotes.length);
+  return quotes[index];
+}
